21ae6a4447ef3237de8e3019d96c6cd7236f04dc
Vseslav Kasatskiy <71323710+samorojy@users.noreply.github.com>
2022-02-28 00:47:14 +0300

    added solution of 1'st task

---
+++ src/hw1/task1/matrix_processing.py
@@ -0,0 +1,33 @@
+from math import prod
+from typing import List
+
+
+def check_matrix_format_correction(*matrices: List[List[int]]):
+    for matrix in matrices:
+        columns_number = len(matrix[0])
+        for row in matrix:
+            if len(row) != columns_number:
+                raise ValueError("Incorrect matrix format")
+
+
+def get_matrices_multiply_product(matrix1: List[List[int]], matrix2: List[List[int]]) -> List[List[int]]:
+    check_matrix_format_correction(matrix1, matrix2)
+    if len(matrix1[0]) != len(matrix2):
+        raise ValueError("Incorrect matrix format for multiplication")
+    return [[sum([prod(elements) for elements in zip(row, column)]) for column in get_transposed_matrix(matrix2)]
+            for row in matrix1]
+
+
+def get_transposed_matrix(matrix: List[List[int]]) -> List[List[int]]:
+    check_matrix_format_correction(matrix)
+    return [list(column) for column in zip(*matrix)]
+
+
+def get_matrices_sum(matrix1: List[List[int]], matrix2: List[List[int]]) -> List[List[int]]:
+    check_matrix_format_correction(matrix1, matrix2)
+    return [[sum(elements) for elements in zip(rows[0], rows[1])] for rows in zip(matrix1, matrix2)]
+
+
+def print_matrix(matrix: List[List[int]]):
+    for row in matrix:
+        print(row)
---
+++ src/hw1/task1/vector_processing.py
@@ -0,0 +1,21 @@
+from math import sqrt, acos
+from typing import List
+
+
+def check_vectors_correction(*vectors: List[int]):
+    for vector in vectors:
+        if len(vector) != len(vectors[0]):
+            raise ValueError("all vectors must be the same size!")
+
+
+def get_vectors_scalar(vector1: List[int], vector2: List[int]) -> int:
+    check_vectors_correction(vector1, vector2)
+    return sum(current_coordinates[0] * current_coordinates[1] for current_coordinates in zip(vector1, vector2))
+
+
+def get_vector_length(vector: List[int]) -> float:
+    return sqrt(sum(current_coordinates ** 2 for current_coordinates in vector))
+
+
+def get_vectors_angle(vector1: List[int], vector2: List[int]) -> float:
+    return acos(get_vectors_scalar(vector1, vector2) / (get_vector_length(vector1) * get_vector_length(vector2)))
