c84cc83bba643361969d82088051d98ed7a07e5c
Vseslav Kasatskiy <71323710+samorojy@users.noreply.github.com>
2022-04-29 14:17:04 +0300

    added task3 solution

---
+++ src/test2/task3.py
@@ -0,0 +1,33 @@
+import re
+from collections import Counter
+from dataclasses import dataclass
+from typing import List
+
+word_pattern = re.compile("[а-яА-Яa-zA-Z]+")
+sentence_pattern = re.compile("[.!?]+")
+
+
+@dataclass(frozen=True)
+class WordStatistic:
+    word: str
+    frequency: int
+
+
+def get_words_statistic(text, top_size) -> List[WordStatistic]:
+    counter = Counter()
+    for word in word_pattern.findall(text):
+        counter[word.lower()] += 1
+    return [WordStatistic(word, freq) for word, freq in counter.most_common(top_size)]
+
+
+def count_sentences(text: str) -> int:
+    return len(sentence_pattern.findall(text))
+
+
+if __name__ == "__main__":
+    with open("text.txt") as file:
+        text = file.read()
+        print(f"There are {count_sentences(text)} sentences in the text")
+        print("Top words:")
+        for word_frequency in get_words_statistic(text, 10):
+            print(f"{word_frequency.word} -> {word_frequency.frequency} times")
---
+++ test/test2/test_task3.py
@@ -0,0 +1,16 @@
+from src.test2.task3 import count_sentences, get_words_statistic, WordStatistic
+
+
+def test_sentence_counter():
+    with open("text.txt") as file:
+        text = file.read()
+        assert 6 == count_sentences(text)
+
+
+def test_words_counter():
+    with open("text.txt") as file:
+        text = file.read()
+        words_statistic = get_words_statistic(text, 3)
+        assert WordStatistic("the", 4) == words_statistic[0]
+        assert WordStatistic("how", 3) == words_statistic[1]
+        assert WordStatistic("with", 3) == words_statistic[2]
