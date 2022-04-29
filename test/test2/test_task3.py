d5399044b02f78f08e81a32bb7d8f61083f6509f
Vseslav Kasatskiy <71323710+samorojy@users.noreply.github.com>
2022-04-29 15:56:12 +0300

    try to fix GithubAction

--- test/test2/test_task3.py
+++ test/test2/test_task3.py
@@ -2,13 +2,13 @@ from src.test2.task3 import count_sentences, get_words_statistic, WordStatistic
 
 
 def test_sentence_counter():
-    with open("text.txt") as file:
+    with open("../../test2/text.txt") as file:
         text = file.read()
         assert 6 == count_sentences(text)
 
 
 def test_words_counter():
-    with open("text.txt") as file:
+    with open("../../test2/text.txt") as file:
         text = file.read()
         words_statistic = get_words_statistic(text, 3)
         assert WordStatistic("the", 4) == words_statistic[0]
