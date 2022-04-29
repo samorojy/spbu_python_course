43e5826704312e0975f632651d3be8ee884222e7
Vseslav Kasatskiy <71323710+samorojy@users.noreply.github.com>
2022-04-29 16:02:31 +0300

    try to fix GithubAction

--- test/test2/test_task3.py
+++ test/test2/test_task3.py
@@ -1,14 +1,16 @@
+import os.path as op
+
 from src.test2.task3 import count_sentences, get_words_statistic, WordStatistic
 
 
 def test_sentence_counter():
-    with open("../../test2/text.txt") as file:
+    with open(op.join(op.dirname(op.realpath(__file__)), "text.txt")) as file:
         text = file.read()
         assert 6 == count_sentences(text)
 
 
 def test_words_counter():
-    with open("../../test2/text.txt") as file:
+    with open(op.join(op.dirname(op.realpath(__file__)), "text.txt")) as file:
         text = file.read()
         words_statistic = get_words_statistic(text, 3)
         assert WordStatistic("the", 4) == words_statistic[0]
